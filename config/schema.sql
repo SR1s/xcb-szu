8a412ab24d05650c159062f945828ceec2cbdba8
SR1s <seemsnevermind@gmail.com>
2014-02-04 02:24:10 +0800

    fix bug: change default value to 1 of columns.parent_id

--- config/schema.sql
+++ config/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `columns` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `order` int(11) NOT NULL DEFAULT '255',
-  `parent_id` int(11) NOT NULL DEFAULT '1',
+  `parent_id` int(11) NOT NULL DEFAULT '0',
   `is_delete` int(1) NOT NULL DEFAULT '0',
   `comments` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
